9b01bc8f800ae135be9f12efcf2ef94cde78f711
«boris-kotov» <barys.kotau@gmail.com>
2025-02-19 02:10:49 +0100

    Completed hm5 tasks 1,2

--- homework-solutions/js/hw-5/task-2.js
+++ homework-solutions/js/hw-5/task-2.js
@@ -17,5 +17,19 @@
  * 100 - делится на 5
  */
 let fizzBuzzResult = '';
+for (let i = 1; i <= 100; i++) {
+  fizzBuzzResult += i;
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    fizzBuzzResult += ' - делится и на 3 и на 5';
+  } else if (i % 3 === 0) {
+    fizzBuzzResult += ' - делится на 3';
+  } else if (i % 5 === 0) {
+    fizzBuzzResult += ' - делится на 5';
+  }
+  if (i < 100) {
+    fizzBuzzResult += `\n`;
+  }
+}
 
 export { fizzBuzzResult };
