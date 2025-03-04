8a1ade9cc4a0e719246e31f9f22d9704bc7653d1
«boris-kotov» <barys.kotau@gmail.com>
2025-03-04 17:45:46 +0100

    done HW 6-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -7,5 +7,13 @@
   Присвойте результат в переменную "unique"
 */
 let unique;
+const toFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+
+for (const item of toFilter) {
+    if (toFilter.indexOf(item) !== toFilter.lastIndexOf(item)) {
+      toFilter.splice(toFilter.lastIndexOf(item), 1)
+    } 
+}
+unique = toFilter
 
 export { unique };
