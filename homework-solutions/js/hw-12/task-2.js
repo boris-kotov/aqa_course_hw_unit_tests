cc13c1cb9349b29623cc2aee815aebfbd709ad65
«boris-kotov» <barys.kotau@gmail.com>
2025-04-02 23:59:23 +0200

    Done task 2 of hw-12

--- homework-solutions/js/hw-12/task-2.js
+++ homework-solutions/js/hw-12/task-2.js
@@ -6,3 +6,33 @@
    Функция должна возвращать полученный объект из респонса
    Обрабатывайте ошибки с помощью try/cath, в конце выведите в консоль текст, что работа функции завершена
  */
+
+async function createTodo(body) {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'post',
+      body: JSON.stringify(body),
+    });
+
+    if (response.status !== 201) {
+      throw new Error(`Response status is ${response.status} which is not 201`);
+    }
+
+    const data = await response.json();
+    console.log(data)
+    // к сожалению, оно возвращает только id
+    Object.keys(body).forEach((key) => {
+      if (body[key] !== data[key]) {
+        throw new Error(`Wrong key received. Expected ${body[key]}, received ${data[key]}`);
+      }
+    });
+
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    console.log('Function execution completed');
+  }
+}
+
+createTodo({ userId: 11, title: 'trying hard', completed: false });
