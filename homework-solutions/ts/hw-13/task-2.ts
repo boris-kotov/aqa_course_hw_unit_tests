307051a495ae0c7da6c738706848399362504ad2
«boris-kotov» <barys.kotau@gmail.com>
2025-04-06 22:59:21 +0200

    Done Task 2 of hw-13

--- homework-solutions/ts/hw-13/task-2.ts
+++ homework-solutions/ts/hw-13/task-2.ts
@@ -8,3 +8,38 @@
 Функция должна возвращать false, если хотя бы одно из условий не выполнено.
 */
 
+/* function validatePassword (password: string) {
+    if (password.length < 8){
+        throw new Error('Password should be at least 8 symbols');
+    }
+    if (!/[A-Z]/.test(password)) {
+        throw new Error('Password should contain at least one capital letter');
+    }
+    if (!/[a-z]/.test(password)) {
+        throw new Error('Password should contain at least one letter in lower case');
+    }
+    if (!/[0-9]/.test(password)) {
+        throw new Error('Password should contain at least one digit');
+    }
+    if ([...password].every(char => char === " ")) {
+        throw new Error('Password should consist of spaces only');
+    }
+    return true;
+}
+// жалко удалять =)
+
+validatePassword(""); */
+
+function validatePassword(password: string) {
+  if (
+    password.trim() !== '' &&
+    password.length >= 8 &&
+    /[A-Z]/.test(password) &&
+    /[a-z]/.test(password) &&
+    /[0-9]/.test(password)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
